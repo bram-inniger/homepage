59a65bea7d690060199101026d615d3d6d209e95
Bram Inniger <bram@inniger.be>
2018-04-17 22:45:28 +0300

    Removed TestContainers as it was adding no extra value

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 Soon to be codebase behind https://inniger.be
 
 ## Build and Run
-1. Install and run Docker
-2. Use docker-compose to start up `docker-compose.yml`, this DB needs to run for Flyway to run, after which jOOQ can do its proper code generation
+1. Install and start the Docker daemon
+2. Use docker-compose to start up `docker-compose.yml`, this fires up the required database
 3. Run `./gradlew clean build` or `./gradlew bootRun` to either test or start the application
-    * Tests will depend on Docker running, but not the docker-compose (though it does not hurt) and uses TestContainers in the background
-    * An actual Application run depends on `docker-compose.yml` running proper for DB access
 
 ## TODO
 * Expand upon this README with proper links and better info
--- src/test/java/be/inniger/homepage/web/TmpControllerTest.java
+++ src/test/java/be/inniger/homepage/web/TmpControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -20,6 +21,8 @@ import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql(statements = "DELETE FROM numbers",
+		executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class TmpControllerTest {
 
 	@Autowired
@@ -36,7 +39,7 @@ public class TmpControllerTest {
 		Stream.of(42L, Long.MIN_VALUE, Long.MAX_VALUE)
 				.forEach(tmpDao::create);
 	}
-	
+
 	@Test
 	public void tmpRealTest() {
 		when(tmpService.tmpList()).thenReturn(tmpDao.list());
