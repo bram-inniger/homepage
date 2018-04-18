120c98a6910cbab045b41d14f5d41728183581a8
Bram Inniger <bram@inniger.be>
2018-04-18 14:16:04 +0300

    Test branch verification. GitHub should now allow merging

--- src/test/java/be/inniger/homepage/web/BranchingTest.java
+++ src/test/java/be/inniger/homepage/web/BranchingTest.java
@@ -1,13 +1,15 @@
 package be.inniger.homepage.web;
 
 import org.junit.Test;
-
-import static org.junit.Assert.fail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BranchingTest {
+
+	private static final Logger log = LoggerFactory.getLogger(BranchingTest.class);
 	
 	@Test
 	public void test() {
-		fail();
+		log.info("All fine!");
 	}
 }
