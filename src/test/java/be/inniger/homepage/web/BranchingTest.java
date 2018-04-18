925439e535c4834105c5889c0fa74c365db0e394
Bram Inniger <bram@inniger.be>
2018-04-18 14:12:32 +0300

    Test branch verification. GitHub should not allow merging as this test is failing.

---
+++ src/test/java/be/inniger/homepage/web/BranchingTest.java
@@ -0,0 +1,13 @@
+package be.inniger.homepage.web;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class BranchingTest {
+	
+	@Test
+	public void test() {
+		fail();
+	}
+}
